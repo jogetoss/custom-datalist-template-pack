ff4ae55a050d09071c3c66478791a75dc560dc87
Rizwan Ahmed <Rizwan Ahmed@DESKTOP-8DK2QEC>
2026-04-21 17:43:01 +0500

    plugin version updated

--- src/main/java/org/joget/marketplace/CustomDataListTemplate.java
+++ src/main/java/org/joget/marketplace/CustomDataListTemplate.java
@@ -56,7 +56,7 @@ public class CustomDataListTemplate extends DataListTemplate {
 
     @Override
     public String getVersion() {
-        return "9.0.1";
+        return "9.0.2";
     }
 
     @Override
